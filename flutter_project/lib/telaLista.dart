26c7f5d3b000d41bde397a0fa1338739f3acbf83
Athos Martinez <athosandrade04@gmail.com>
2024-04-13 19:47:18 -0300

    Adicionado metodo para salvar as alterações

--- flutter_project/lib/telaLista.dart
+++ flutter_project/lib/telaLista.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ListaScreen extends StatefulWidget {
   final String nome;
@@ -10,8 +11,30 @@ class ListaScreen extends StatefulWidget {
 }
 
 class _ListaScreenState extends State<ListaScreen> {
-  final List<String> _itemDescriptions = List<String>.generate(20, (index) => 'Descrição do item ${index + 1}');
-  final List<String> _itemTitles = List<String>.generate(20, (index) => 'Título ${index + 1} da lista');
+  List<String> _itemDescriptions = [];
+  List<String> _itemTitles = [];
+  bool _isLoading = true; // Flag para indicar se os dados ainda estão sendo carregados
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _itemTitles = prefs.getStringList('itemTitles') ?? List<String>.generate(20, (index) => 'Título ${index + 1} da lista');
+      _itemDescriptions = prefs.getStringList('itemDescriptions') ?? List<String>.generate(20, (index) => 'Descrição do item ${index + 1}');
+      _isLoading = false; // Dados carregados, remova o indicador de carregamento
+    });
+  }
+
+  Future<void> _saveData() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('itemTitles', _itemTitles);
+    await prefs.setStringList('itemDescriptions', _itemDescriptions);
+  }
 
   void _showDialog(BuildContext context, int itemNumber) {
     TextEditingController _descriptionController = TextEditingController(text: _itemDescriptions[itemNumber - 1]);
@@ -38,11 +61,12 @@ class _ListaScreenState extends State<ListaScreen> {
           actions: <Widget>[
             TextButton(
               child: const Text('Salvar'),
-              onPressed: () {
+              onPressed: () async {
                 setState(() {
                   _itemTitles[itemNumber - 1] = _titleController.text;
                   _itemDescriptions[itemNumber - 1] = _descriptionController.text;
                 });
+                await _saveData();
                 Navigator.of(context).pop(); // Fecha o AlertDialog
               },
             ),
@@ -64,18 +88,20 @@ class _ListaScreenState extends State<ListaScreen> {
       appBar: AppBar(
         title: Text('Bem vindo(a), ${widget.nome}'),
       ),
-      body: ListView.builder(
-        itemCount: 20,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text(_itemTitles[index]),
-              subtitle: Text(_itemDescriptions[index]),
-              onTap: () => _showDialog(context, index + 1), // Chama a função _showDialog quando um item é tocado.
-            ),
-          );
-        },
-      ),
+      body: _isLoading
+        ? Center(child: CircularProgressIndicator()) // Mostra um indicador de carregamento enquanto os dados não são carregados
+        : ListView.builder(
+            itemCount: 20,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  title: Text(_itemTitles[index]),
+                  subtitle: Text(_itemDescriptions[index]),
+                  onTap: () => _showDialog(context, index + 1), // Chama a função _showDialog quando um item é tocado.
+                ),
+              );
+            },
+          ),
     );
   }
 }
